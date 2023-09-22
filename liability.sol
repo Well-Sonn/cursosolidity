799b5e3c7a5cb72e969088e07285ae83719bd933
Wellerson Barauna <143764076+Well-Sonn@users.noreply.github.com>
2023-09-22 16:55:40 -0300

    Update liability.sol

--- liability.sol
+++ liability.sol
@@ -1,45 +1,49 @@
-//0xc57E3F58cA5355b388FeDB591ffDEE9AdA002123 - contract
+// SPDX-License-Identifier: CC-BY-4.0
+// (c) Desenvolvido por Matheus Ricardo Tomas
+// This work is licensed under a Creative Commons Attribution 4.0 International License.
 
-// SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "contracts/exercicio_dev_token.sol";
+import "https://github.com/jeffprestes/cursosolidity/blob/master/bradesco_token_aberto.sol";
 
-contract TokenLiabity {
+contract liability {
+    ExercicioToken private exercicioToken;
 
-    struct Client {
-
-        address clientAddress; 
-        uint256 tokenBalance;  
+    struct Cliente {
+        string primeiroNome;
+        string sobreNome;
+        address payable endereco;
+        bytes32 hashConta;
+        bool existe;
     }
 
-    mapping(address => Client) public clients; 
+    Cliente private cliente;
 
-    ExercicioToken public tokenContract; 
+    constructor(
+        string memory _primeiroNome,
+        string memory _sobreNome,
+        string memory _agencia,
+        string memory _conta
+    ) {
+        exercicioToken = ExercicioToken(0x89A2E711b2246B586E51f579676BE2381441A0d0);
 
-    constructor() {
-        tokenContract = ExercicioToken(0x89A2E711b2246B586E51f579676BE2381441A0d0);
+        string memory strTemp = string(abi.encodePacked(_agencia, _conta));
+        bytes32 hashTemp = keccak256(abi.encodePacked(strTemp));
+
+        cliente = Cliente(_primeiroNome, _sobreNome, payable(msg.sender), hashTemp, true);
+
+        gerarTokenParaEuCliente(10000); 
     }
 
-    
-    function MeuSaldo() public view returns (uint256) {
-        address clientAddress = msg.sender;
-        return tokenContract.balanceOf(clientAddress);
+    function meuSaldo() public view returns (uint256) {
+        return exercicioToken.balanceOf(address(this));
     }
 
-    
-    function TokenParaEuCliente(uint256 amount) public returns (bool) {
-        address clientAddress = msg.sender;
-        require(clientAddress != address(0), "Endereco de cliente invalido");
-        require(amount > 0, "O valor de transferencia deve ser maior que 0");
-        
-        
-        require(tokenContract.mint(clientAddress, amount), "Erro: Nao e possivel criar novos saldos");
-        
-        clients[clientAddress].tokenBalance += amount;
+    function gerarTokenParaEuCliente(uint256 _amount) public returns (bool) {
+        return exercicioToken.mint(address(this), _amount);
     }
 
-    function MeuEndereco() public view returns (address) {
-        return address(this);
+    function transferirTokens(address endereco, uint256 _valor) public returns (bool) {
+        return exercicioToken.transfer(endereco, _valor);
     }
 }
